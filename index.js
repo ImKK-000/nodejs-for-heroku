f692c8a36830df478c4710fc0e1ff761e104fd5e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-03 19:28:18 +0700

    :tada: Add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const PORT = 3000
+
+app
+  .use(bodyParser.urlencoded({ extended: false }))
+  .get('/', (req, res) => res.send('hello world by nodejs at heroku!'))
+  .get('/api', (req, res) =>
+    res.send([
+      { object: 'john', type: 'string' },
+      { object: 10, type: 'number' },
+      { object: true, type: 'boolean' }
+    ])
+  )
+  .listen(PORT, () => console.log('listen on port ' + PORT))
